da552fc06cef540a99253bc754abeffe7abb3e3c
epatters <ejpatters@gmail.com>
2018-04-14 20:37:26 -0500

    ENH: Add functions to get package of function/object, if possible.

--- R/inspect.R
+++ R/inspect.R
@@ -1,4 +1,3 @@
-
 #' Class system of object
 #' 
 #' @description Which class system does the object use?
@@ -10,4 +9,53 @@ class_system <- function(x) {
     tryCatch({ x$getRefClass; "R5"}, error=function(e) "S4")
   else
     "S3"
+}
+
+#' Get package of function
+#' 
+#' @description In which package is the function defined?
+#' 
+#' @details This function reliably determines the definining package of a
+#' function object, including for primitive functions, anonymous functions,
+#' and methods.
+#' 
+#' @seealso \code{\link{obj_package}}
+fun_package <- function(fun) {
+  stopifnot(is.function(fun))
+  if (is.primitive(fun))
+    # Special case: primitive functions do not belong to an environment.
+    return("base")
+  
+  env = environment(fun)
+  if (is(fun, "refMethodDef"))
+    # Special case: class method of R5 class.
+    return(attr(env$def, "package"))
+  
+  else if (is.R6Class(env))
+    # Special case: class method of R6 class.
+    env = env$parent_env
+
+  # General case: find the nearest named environment.
+  name = ""
+  while (name == "") {
+    name = environmentName(env)
+    env = parent.env(env)
+  }
+  name
+}
+
+#' Get package of object
+#' 
+#' @description In which package is the object's class defined?
+#' 
+#' @details This function tries to determine the defining package of the
+#' object's class. In general, this is not possible because R has an informal
+#' class system.
+#' 
+#' Currently, this function works reliably on S4 and R5 instances, but not on
+#' S3 or R6 instances. When it fails, it returns \code{NULL}.
+#' 
+#' @seealso \code{\link{fun_package}}
+obj_package <- function(x) {
+  attr(class(x), "package")
 }
--- tests/testthat/test_inspect.R
+++ tests/testthat/test_inspect.R
@@ -14,3 +14,23 @@ test_that("get class system of object", {
   expect_equal(class_system(dict()), "S3")
   expect_equal(class_system(ordered_dict()), "R6")
 })
+
+test_that("get package of function", {
+  expect_equal(fun_package(is.function), "base") # primitive
+  expect_equal(fun_package(is.primitive), "base") # not primitive
+  expect_equal(fun_package(lm), "stats")
+  
+  pkg = packageName()
+  expect_equal(fun_package(ordered_dict), pkg)
+  expect_equal(fun_package(ordered_dict_class$new), pkg)
+  expect_equal(fun_package(ordered_dict_class$new()$clone), pkg)
+  expect_equal(fun_package(ExampleClass), pkg)
+  expect_equal(fun_package(ExampleRefClass$new), pkg)
+})
+
+test_that("get package of object", {
+  pkg = packageName()
+  expect_equal(obj_package(dict()), NULL)
+  expect_equal(obj_package(ExampleClass()), pkg)
+  expect_equal(obj_package(ExampleRefClass$new()), pkg)
+})
