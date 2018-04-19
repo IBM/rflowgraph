7fc2d70914970a14608a895a163e26e4f9401c2b
epatters <ejpatters@gmail.com>
2018-04-19 15:30:06 -0500

    TST: Record parenthesized arithmetic expression.

--- tests/testthat/test_record.R
+++ tests/testthat/test_record.R
@@ -20,3 +20,19 @@ test_that("record trivial arithemetic expression", {
   })
   expect_equal(h, g)
 })
+
+test_that("record parenthesized arithmetic expression", {
+  g = wiring_diagram()
+  add_node(g, "numeric:1", list(), ret)
+  add_node(g, "numeric:2", list(), ret)
+  add_node(g, "numeric:3", list(), ret)
+  add_node(g, "+:1", c("e1","e2"), ret)
+  add_edge(g, "numeric:2", "+:1", ret, "e1")
+  add_edge(g, "numeric:3", "+:1", ret, "e2")
+  add_node(g, "*:1", c("e1","e2"), ret)
+  add_edge(g, "numeric:1", "*:1", ret, "e1")
+  add_edge(g, "+:1", "*:1", ret, "e2")
+  
+  h = record(2*(1+1))
+  expect_equal(h, g)
+})
